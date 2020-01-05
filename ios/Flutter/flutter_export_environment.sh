e21b4ee35f25243123b0ed4dc9ccbb04acead918
muckustask <muckustask@gmail.com>
2020-01-04 23:35:22 -0600

    Creando funciones para crear e insertar en la base de datos

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\El Mike\Desktop\Flutter\flutter_qrscanner_FH"
+export "FLUTTER_APPLICATION_PATH=C:\Users\The Mucky\Desktop\FLUTTER\flutter_qrscanner_FH"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,25 +1,77 @@
+import 'dart:io';
+
+import 'package:flutter_qrscanner_fh/src/models/scann_model.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 
 class DBProvider {
 
+  // Propiedad privada
   static Database _database;
+  // Constructor privado
   static final DBProvider db = DBProvider._();
 
   DBProvider._();
 
-  Future get database async {
+  Future<Database> get database async {
 
     // En caso de que haya una instancia de base de datos
     if(database != null) return _database;
 
     // En caso de que no haya una instancia de base de datos la creamos
     _database = await initDB();
+    return _database;
 
   }
 
   initDB() async {
+    // Obtiene el path de donde se encuantra la base de datos, esto es solo donde se puese escribir
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+
+    // Obtenemos el path completo incluyendo el nombre del archivo
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+
+    // Comandos para crear la base de datos
     
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          'CREATE TABLE Scans ('
+          ' id INTEGER PRIMARY KEY,'
+          ' tipo TEXT,'
+          ' valor TEXT'
+          ')'
+        );
+      }
+    );
   }
+
+  // Crear registros de base de datos
+  nuevoScanRaw(ScanModel nuevoScan) async {
+
+    final db = await database;
+
+    final res = await db.rawInsert(
+      "INSERT INTO Scans (id, tipo, valor) "
+      "VALUES (${nuevoScan.id}, '${nuevoScan.tipo}', '${nuevoScan.valor}')"
+    );
+
+    return res;
+
+  }
+
+  // La forma que se esta utilizando para insertar
+  nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+
+    final res = await db.insert('Scans', nuevoScan.toJson());
+    return res;
+  }
+
 
 }
