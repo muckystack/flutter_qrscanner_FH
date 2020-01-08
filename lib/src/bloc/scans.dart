484bb7c1e63aa05f6c9169c61a3905a88d4d563e
muckustask <muckustask@gmail.com>
2020-01-07 21:40:00 -0600

    Manejando streams

---
+++ lib/src/bloc/scans.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:flutter_qrscanner_fh/src/providers/db_provider.dart';
+
+class ScansBloc {
+  
+  static final ScansBloc _singleton = new ScansBloc._internal();
+
+  factory ScansBloc() {
+    return _singleton;
+  }
+
+  ScansBloc._internal() {
+    // Obtener los scans de la base de datos
+
+  }
+
+  final _scansController = StreamController<List<ScanModel>>.broadcast();
+
+  Stream<List<ScanModel>> get scansStream => _scansController.stream;
+
+
+  dispose() {
+    _scansController?.close();
+  }
+
+}
